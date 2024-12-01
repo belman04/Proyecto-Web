1559ca594c76808696926d33225d0c56f2158df7
belman04 <a21490528@itmexicali.edu.mx>
2024-11-30 16:10:35 -0800

    modificaciones a config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 3000;
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+const DB_HOST = process.env.DB_HOST || 'autorack.proxy.rlwy.net';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || '1234';
-const DB_NAME = process.env.DB_NAME || 'restaurante';
-const DB_PORT = process.env.DB_PORT || 3306;
+const DB_PASSWORD = process.env.DB_PASSWORD || 'aCGUwsecPYJKEBStaXTYeoClxZSGONeu';
+const DB_NAME = process.env.DB_NAME || 'railway';
+const DB_PORT = process.env.DB_PORT || 26029;
 
 module.exports = {
   PORT,
